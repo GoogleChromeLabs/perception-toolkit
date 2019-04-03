2312a5a2d7664ff8956223bccd4619c926d19631
Matt Small <mbsmall@google.com>
2019-04-03 15:45:13 -0700

    add license to bundled files

--- rollup.config.js
+++ rollup.config.js
@@ -18,6 +18,8 @@
 import typescript from 'rollup-plugin-typescript2';
 import loadz0r from './build/dynamic-loader/index.js';
 import { terser } from 'rollup-plugin-terser';
+import license from 'rollup-plugin-license';
+
 
 export default [{
   input: [
@@ -48,7 +50,27 @@ export default [{
         return `${root}/lib/bundled`;
       }
     }),
-    terser()
+    terser(),
+    license({
+      sourcemap: true,
+
+      banner: `
+@license
+Copyright 2019 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+      `
+    })
   ],
 
   output: {
